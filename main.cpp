e14e2b2f99e4db818ff61f4d621438adcfea2b8c
Bradley Seibert <60272855+bradseibert@users.noreply.github.com>
2020-04-16 16:20:35 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,26 @@
+
+
+#include <iostream>
+#include "battleship.h"
+#include "Dolthy.h"
+#include "Jalthy.h"
+#include "Malthy.h"
+using namespace std;
+
+int main()
+{
+    cout << "Hello World" << endl;
+    cout << "----------------" << endl;
+    battleship* arrayOfShips[3];
+    arrayOfShips[0] = new Malthy(44);
+    arrayOfShips[1] = new Jalthy(22);
+    arrayOfShips[2] = new Dolthy(22);
+
+    for (int i = 0; i < 3; i++)
+    {
+        arrayOfShips[i]->fire();
+    }
+    //battleship ship(44); //error because cannot make an object from abstract class.//
+}
+
+
